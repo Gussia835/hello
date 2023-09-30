dce208966723ac2eab68aedb8c0f6c317b641d40
local user <user@mil8a-118b-10.mmcs.sfedu.ru>
2023-09-30 09:35:09 +0300

    ваше сообщение

---
+++ time.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var year := ReadInteger('year =');
+  Assert(year > 0);
+  if (year mod 4 = 0) and (year mod 100 = 0 and not(year mod 400 = 0)) then
+  begin
+    Print('високосный год');
+  end
+  else
+    Print('не високосный');
+end.
