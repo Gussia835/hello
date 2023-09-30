ca108fd782c90dfda6d1ef65093e51bf851f1237
local user <user@mil8a-118b-10.mmcs.sfedu.ru>
2023-09-30 09:29:28 +0300

    Ваше сообщение

--- Hello.pas
+++ Hello.pas
@@ -1,4 +1,11 @@
 ﻿//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
 begin
-  Print('Hello world');
+  var currTime := ReadInteger('Который сейчас час?');
+  case currTime of
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый день, мир!');
+    17..22: Println('Добрый вечер, мир!');
+    else Println('Доброй ночи, мир!');
+    end;
+  
 end.
